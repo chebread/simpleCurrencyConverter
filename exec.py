58498dc30683d79827315c1acc1a6872af7db7ad
차한음 <ihaneum@chahan-eum-ui-MacBookAir.local>
2021-05-19 12:23:50 +0900

    version: 1

---
+++ exec.py
@@ -0,0 +1,17 @@
+# 간단 환율계산 (달러)
+from datetime import *
+
+clock = datetime.now()
+ymd = "%s 년 %s 월 %s 일"%(clock.year, clock.month, clock.day)
+def Conversion(v, c):
+    return v * c # 환율
+
+print("-- 간단 환율 계산 (달러) --")
+print("-> 환율: %s 기준 -<"%ymd)
+while True:
+    value = float(input("달러(0:quit): "))
+    if value == 0.0:
+        print("=> 종료 <=")
+        break
+    _won_ = Conversion(value, _usd_)
+    print("-- Dollar %f = won %f --"%(value, _won_))
